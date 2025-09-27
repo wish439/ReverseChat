1eac73b1c6c33019df9d348a46c5097a7c079131
wishtoday238 <wishtoday238@gmail.com>
2025-09-28 00:03:32 +0800

    init

--- src/main/java/org/wishtoday/ps/reverseChat/ReverseChat.java
+++ src/main/java/org/wishtoday/ps/reverseChat/ReverseChat.java
@@ -1,17 +1,36 @@
 package org.wishtoday.ps.reverseChat;
 
+import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
+import org.bukkit.NamespacedKey;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.wishtoday.ps.reverseChat.Command.MainCommand;
+import org.wishtoday.ps.reverseChat.Events.PlayerChatEvent;
 
 public final class ReverseChat extends JavaPlugin {
+    public static NamespacedKey REVERSED;
+
+    private static ReverseChat instance;
+
+    public static final String NAME = "[ReverseChat]";
+
+    public static ReverseChat getInstance() {
+        return instance;
+    }
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        instance = this;
+        REVERSED = NamespacedKey.fromString("reversed", this);
+        //noinspection UnstableApiUsage
+        this.getLifecycleManager().registerEventHandler(
+                LifecycleEvents.COMMANDS, event -> {
+                    MainCommand.register(event.registrar());
+                }
+        );
+        this.getServer().getPluginManager().registerEvents(new PlayerChatEvent(), this);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
     }
 }
